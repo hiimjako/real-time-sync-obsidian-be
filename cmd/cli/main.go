5972762ff222078ce2c81eb47eba6bfc3594349c
Alberto Moretti <moretti919@gmail.com>
2024-10-18 00:01:17 +0200

    feat: add POST in cli

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/hiimjako/real-time-sync-obsidian-be/pkg/diff"
 	"github.com/hiimjako/real-time-sync-obsidian-be/pkg/screen"
 )
 
 var (
-	fileId = flag.String("file", uuid.NewString(), "file to open")
+	serverURL = flag.String("url", "127.0.0.1:8080", "server URL")
+	fileId    = flag.String("file", uuid.NewString(), "file to open")
 )
 
 func main() {
@@ -27,6 +34,8 @@ func main() {
 		errc <- screen.Init()
 	}()
 
+	go pollText(&screen)
+
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt)
 	select {
@@ -35,4 +44,55 @@ func main() {
 	case sig := <-sigs:
 		log.Printf("terminating: %v", sig)
 	}
+}
+
+func pollText(s *screen.Screen) {
+	lastContent := ""
+	go func() {
+		for {
+			<-time.After(10 * time.Millisecond)
+
+			content := s.Content()
+			d := diff.ComputeDiff(content, lastContent)
+
+			if len(d) == 0 {
+				continue
+			}
+
+			err := sendChunk(d)
+			if err != nil {
+				log.Println(err)
+			}
+
+			lastContent = content
+		}
+	}()
+}
+
+func sendChunk(data []diff.DiffChunk) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	url := *serverURL + "/publish/" + *fileId
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error in request: %v", resp)
+	}
+
+	return nil
 }
